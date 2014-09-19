a187cfad51a83bfee998f380560254d9678e0e5a
Roslarb <kristoffer.mikkelsen@hotmail.com>
2014-09-19 09:21:21 +0200

    Oprettet Consumer og Producer klasser

--- ProducerConsumer/Consumer.cs
+++ ProducerConsumer/Consumer.cs
@@ -5,19 +5,6 @@ namespace ProducerConsumer
 {
     class Consumer
     {
-        private BoundedBuffer buf;
-        public Consumer(BoundedBuffer obj)
-        {
-            buf = obj;
-        }
-        public void Run()
-        {
-            if (buf.MyQueue.Count != 0)
-            {
-                buf.Take();
-                Console.WriteLine("Line removed");
-            }           
-        }
-
+        
     }
 }
